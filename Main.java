07f7fbb3e9322bf1873fdb2872cd7acf57ea929f
skirios <skiriosfoto@gmail.com>
2021-03-05 13:13:51 +0300

    added .gitignore Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[]agrs){
+        int money = 2000000000000;
+        int transfer = 5000000000;
+        int total = money + transfer;
+        System.out.println(total);
+    }
+
+}
