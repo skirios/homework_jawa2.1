af50a11fc5f18d1f07867215880c16bbc3605502
skirios <skiriosfoto@gmail.com>
2021-03-05 16:48:48 +0300

    added _

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[]agrs){
-        int money = 2000000000000;
-        int transfer = 5000000000;
+        int money = 2_000_000_000;
+        int transfer = 500_000_000;
         int total = money + transfer;
         System.out.println(total);
     }
